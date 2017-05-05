Just need to correct the problem with normalization

categorizer.py
@@ -16,18 +16,18 @@ def model_training(model_name, verbose=global_verbosity):
         directory = 'saves/common_no_discr/'
         suffix = '_no_discr'
 
-    training_set = np.loadtxt(directory + 'full_training_set')
-    training_labels = np.loadtxt(directory + 'full_training_labels')
+    training_set = np.loadtxt(directory + 'full_training_set.txt')
+    training_labels = np.loadtxt(directory + 'full_training_labels.txt')
 
     if model_name.split('_')[-1] == 'invfreq':
-        weights = np.loadtxt('full_training_weights' + suffix)
+        weights = np.loadtxt(directory + 'full_training_weights.txt')
         analyser.fit(training_set, training_labels, 1./weights)
     elif model_name.split('_')[-1] == 'purity':
-        custom_weights = np.array([3., 0.5, 0.5, 0.5])
+        custom_weights = np.array([3., 0.5, 0.5,  0.5, 0.5])
         weights = np.array([custom_weights[int(cat)] for cat in training_labels])
         analyser.fit(training_set, training_labels, weights)
     elif model_name.split('_')[-1] == 'content':
-        custom_weights = np.array([1., 3., 2., 0.5])
+        custom_weights = np.array([1., 3.,0.5,  2., 0.5])
         weights = np.array([custom_weights[int(cat)] for cat in training_labels])
         analyser.fit(training_set, training_labels, weights)
     else:
@@ -44,8 +44,8 @@ def model_training(model_name, verbose=global_verbosity):
     with open('saves/' + model_name + suffix + '/categorizer.txt', mode='wb') as file:
         pickle.dump(analyser, file)
 
-    test_set = np.loadtxt(directory + 'full_test_set')
-    test_labels = np.loadtxt(directory + 'full_training_labels')
+    test_set = np.loadtxt(directory + 'full_test_set.txt')
+    test_labels = np.loadtxt(directory + 'full_test_labels.txt')
     logging.info('Training score : ' + str(analyser.score(training_set, training_labels)))
     logging.info('Generalization score : ' + str(analyser.score(test_set, test_labels)))
 
@@ -58,7 +58,7 @@ def generate_predictions(model_name, tolerance=0., verbose=global_verbosity):
         directory = 'saves/common_no_discr/'
         suffix = '_no_discr'
 
-    scaled_dataset = np.loadtxt(directory + 'test_set_scaled.txt')
+    scaled_dataset = np.loadtxt(directory + 'full_test_set.txt')
 
     with open('saves/' + model_name + suffix + '/categorizer.txt', mode='rb') as file:
         classifier = pickle.load(file)
@@ -74,7 +74,7 @@ def generate_predictions(model_name, tolerance=0., verbose=global_verbosity):
             pass
 
     out_path = 'saves/' + model_name + suffix
-    np.savetxt(out_path + 'predictions.txt', results)
+    np.savetxt(out_path + '/predictions.txt', results)
 
     if verbose:
         print(out_path + ' predictions successfully stored')

plotter.py
@@ -39,11 +39,11 @@ def content_plot(model_name, permutation=None, save=True, verbose=global_verbosi
     weights = np.loadtxt('saves/common' + suffix + 'full_test_weights.txt')
     predictions = np.loadtxt(directory + 'predictions.txt')
 
-    nb_categories = max(len(np.unique(np.loadtxt('saves/' + directory + 'ggH_predictions.txt'))), 5)
+    nb_categories = 5  # max(len(np.unique(np.loadtxt('saves/' + directory + 'ggH_predictions.txt'))), 5)
     contents_table = np.zeros((nb_categories, len(event_categories)))
 
     for true_tag, predicted_tag, rescaled_weight in izip(true_categories, predictions, weights):
-        contents_table[predicted_tag, true_tag] += weights
+        contents_table[predicted_tag, true_tag] += rescaled_weight
 
 
     ordering = range(nb_categories)
@@ -59,11 +59,11 @@ def content_plot(model_name, permutation=None, save=True, verbose=global_verbosi
         position = ordering[category]
         normalized_content = contents_table[category, :].astype('float') / np.sum(contents_table[category, :])
         tmp = 0.
-        for gen_mode in range(len(production_modes)):
+        for gen_mode in range(nb_categories):
             if position == 1:
                 ax.axhspan(position * 0.19 + 0.025, (position + 1) * 0.19 - 0.025, tmp,
                            tmp + normalized_content[gen_mode],
-                           color=color_array[gen_mode], label=production_modes[gen_mode])
+                           color=color_array[gen_mode], label=event_categories[gen_mode])
             else:
                 ax.axhspan(position * 0.19 + 0.025, (position + 1) * 0.19 - 0.025, tmp,
                            tmp + normalized_content[gen_mode],
@@ -81,25 +81,30 @@ def content_plot(model_name, permutation=None, save=True, verbose=global_verbosi
 
 
 if __name__ == "__main__":
-    for use_calculated_features in [True, False]:
-        suff = '/'
-        if not use_calculated_features:
-            suff = '_no_discr/'
-        for model_name in models_dict.keys():
-            logging.info('Studying model ' + model_name + '\n')
+    suff = ''
+    if not use_calculated_features:
+        suff = '_no_discr'
 
-            if not os.path.isdir('figs/tmp'):
-                os.makedirs('figs/tmp')
-            if not os.path.isfile('saves/common' + suff + 'full_training_set.txt'):
-                pr.full_process()
 
-            try:
-                open('saves/' + model_name + suff + '/categorizer.txt', 'rb')
-            except IOError:
-                logging.info('Training model ' + model_name)
-                ctg.model_training(model_name)
+    #pr.full_process()
+    for model_name in models_dict.keys():
+        logging.info('Studying model ' + model_name)
+	
+        if not os.path.isdir('figs/tmp'):
+            os.makedirs('figs/tmp')
+            
+        try:
+            open('saves/' + model_name + suff + '/categorizer.txt', 'rb')
+        except IOError:
+            logging.info('Training model ' + model_name)
+            ctg.model_training(model_name)
+        try:
+            open('saves/' + model_name + suff + '/predictions.txt', 'rb')
+        except IOError:
+            logging.info('Generating predictions for ' + model_name + suff)
+            ctg.generate_predictions(model_name)
 
-            content_plot(model_name)
+        content_plot(model_name)
 
 
 

preprocessing.py
@@ -195,8 +195,6 @@ def make_scaled_datasets():
                            cross_sections[filename.split('/')[-1]] / event_numbers[filename.split('/')[-1]]
             training_set = np.concatenate((training_set, temp_train), axis=0)
             test_set = np.concatenate((test_set, temp_test), axis=0)
-            np.savetxt(filename + '_test_scaled.txt', temp_test)
-            np.savetxt(filename + '_test_weights_scaled.txt', tmp_test_weights)
             training_labels = np.concatenate((training_labels, (idx + 1) * np.ones(np.ma.size(temp_train, 0))), axis=0)
             test_labels = np.concatenate((test_labels, (idx + 1) * np.ones(np.ma.size(temp_test, 0))), axis=0)
             training_weights = np.concatenate((training_weights, tmp_training_weights), axis=0)
@@ -219,16 +217,16 @@ def clean_intermediate_files():
 
 
 def full_process():
-    logging.info('\n Reading root files \n')
+    logging.info('Reading root files')
     read_root_files()
-    logging.info('\n Merging vector modes \n')
+    logging.info('Merging vector modes')
     merge_vector_modes()
-    logging.info('\n Preparing scalers \n')
+    logging.info('Preparing scalers')
     prepare_scalers()
-    logging.info('\n Merging and scaling datasets \n')
+    logging.info('Merging and scaling datasets')
     make_scaled_datasets()
-    # logging.info('\n Removing all intermediate files \n')
-    # clean_intermediate_files()
+    logging.info('Removing all intermediate files')
+    clean_intermediate_files()
 
 if __name__ == '__main__':
     full_process()