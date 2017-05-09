7bb0cf616036019cad54281fef2ba2a4002b1f85
Arnaud Fanthomme <afanthomme@gmail.com>
2017-05-09 15:41:09 +0200

    Not working

--- main.py
+++ main.py
@@ -15,37 +15,36 @@ if ignore_warnings:
 
 if __name__ == "__main__":
 
-    for features_set_selector in range(3):
-        if features_set_selector == 0:
-            directory = 'saves/common_no_discr/'
-            suffix = ''
-        elif features_set_selector == 1:
-            directory = 'saves/common_only_discr/'
-            suffix = '_no_discr'
-        elif features_set_selector == 2:
-            directory = 'saves/common/'
-            suffix = '_no_discr'
-        else:
-            raise IOError
+    if features_set_selector == 0:
+        directory = 'saves/common_no_discr/'
+        suffix = ''
+    elif features_set_selector == 1:
+        directory = 'saves/common_only_discr/'
+        suffix = '_no_discr'
+    elif features_set_selector == 2:
+        directory = 'saves/common/'
+        suffix = '_no_discr'
+    else:
+        raise IOError
 
-        if not tests.common_saves_found():
-            pr.full_process()
+    if not tests.common_saves_found():
+        pr.full_process()
 
-        if not os.path.isdir('figs/tmp'):
-            os.makedirs('figs/tmp')
+    if not os.path.isdir('figs/tmp'):
+        os.makedirs('figs/tmp')
 
-        for model_name in models_dict.keys():
-            logging.info('Studying model ' + model_name + suffix)
-            try:
-                open('saves/' + model_name + suffix + '/categorizer.pkl', 'rb')
-            except IOError:
-                logging.info('Training model ' + model_name)
-                ctg.model_training(model_name)
-            try:
-                open('saves/' + model_name + suffix + '/predictions.txt', 'rb')
-            except IOError:
-                logging.info('Generating predictions for ' + model_name + suffix)
-                ctg.generate_predictions(model_name)
+    for model_name in models_dict.keys():
+        logging.info('Studying model ' + model_name + suffix)
+        try:
+            open('saves/' + model_name + suffix + '/categorizer.pkl', 'rb')
+        except IOError:
+            logging.info('Training model ' + model_name)
+            ctg.model_training(model_name)
+        try:
+            open('saves/' + model_name + suffix + '/predictions.txt', 'rb')
+        except IOError:
+            logging.info('Generating predictions for ' + model_name + suffix)
+            ctg.generate_predictions(model_name)
 
-            content_plot(model_name)
+        content_plot(model_name)
     logging.info('All models studied with features set ' + suffix)
