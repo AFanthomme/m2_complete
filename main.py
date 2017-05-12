fabf910ca63b98264533cf3b6b1be3594bea1432
Arnaud <afanthomme@gmail.com>
2017-05-12 15:09:54 +0200

    Tests

--- main.py
+++ main.py
@@ -17,45 +17,45 @@ if ignore_warnings:
     warnings.filterwarnings('ignore')
 
 
-def main():
-    model_name = 'adaboost_stumps_300_purity'
-    directory, suffix = dir_suff_dict[features_set_selector]
-    scaled_dataset = np.loadtxt(directory + 'full_test_set.txt')
-
-    with open('saves/' + model_name + suffix + '/categorizer.pkl', mode='rb') as file:
-        classifier = pickle.load(file)
-
-    out_path = 'saves/' + model_name + suffix
-    probas = classifier.predict_proba(scaled_dataset)
-    np.savetxt(out_path + '/probas.txt', probas)
-
-main()
-
-
-# if __name__ == "__main__":
-#
+# def main():
+#     model_name = 'adaboost_stumps_300_purity'
 #     directory, suffix = dir_suff_dict[features_set_selector]
+#     scaled_dataset = np.loadtxt(directory + 'full_test_set.txt')
 #
-#     if not (tests.common_saves_found() and tests.lengths_consistent()):
-#         pass #pr.full_process()
-#     if not (tests.common_saves_found() and tests.lengths_consistent()):
-#         raise UserWarning
+#     with open('saves/' + model_name + suffix + '/categorizer.pkl', mode='rb') as file:
+#         classifier = pickle.load(file)
 #
-#     if not os.path.isdir('figs/tmp'):
-#         os.makedirs('figs/tmp')
+#     out_path = 'saves/' + model_name + suffix
+#     probas = classifier.predict_proba(scaled_dataset)
+#     np.savetxt(out_path + '/probas.txt', probas)
 #
-#     for model_name in models_dict.keys():
-#         logging.info('Studying model ' + model_name + suffix)
-#         try:
-#             open('saves/' + model_name + suffix + '/categorizer.pkl', 'rb')
-#         except IOError:
-#             logging.info('Training model ' + model_name)
-#             ctg.model_training(model_name)
-#         try:
-#             open('saves/' + model_name + suffix + '/predictions.txt', 'rb')
-#         except IOError:
-#             logging.info('Generating predictions for ' + model_name + suffix)
-#             ctg.generate_predictions(model_name)
-#
-#         content_plot(model_name)
-#     logging.info('All models studied with features set ' + suffix)
+# main()
+
+
+if __name__ == "__main__":
+
+    directory, suffix = dir_suff_dict[features_set_selector]
+
+    if not (tests.common_saves_found() and tests.lengths_consistent()):
+        pass #pr.full_process()
+    if not (tests.common_saves_found() and tests.lengths_consistent()):
+        raise UserWarning
+
+    if not os.path.isdir('figs/tmp'):
+        os.makedirs('figs/tmp')
+
+    for model_name in models_dict.keys():
+        logging.info('Studying model ' + model_name + suffix)
+        try:
+            open('saves/' + model_name + suffix + '/categorizer.pkl', 'rb')
+        except IOError:
+            logging.info('Training model ' + model_name)
+            ctg.model_training(model_name)
+        try:
+            open('saves/' + model_name + suffix + '/predictions.txt', 'rb')
+        except IOError:
+            logging.info('Generating predictions for ' + model_name + suffix)
+            ctg.generate_predictions(model_name)
+
+        content_plot(model_name)
+    logging.info('All models studied with features set ' + suffix)
